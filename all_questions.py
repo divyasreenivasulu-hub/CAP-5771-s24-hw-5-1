b40d9c1f946805f052d9892924e96148f8951075
AnkithaSreeramoju <83494220+AnkithaSreeramoju@users.noreply.github.com>
2024-04-15 16:38:23 -0400

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -54,11 +54,11 @@ def question2():
     # The formulas should only use the variable 'p'. The formulas should be
     # a valid Python expression. Use the functions in the math module as
     # required.
-    answers['(c) Weight update'] = None
+    answers['(c) Weight update'] = "0.424p"
 
     # type: float
     # the answer should be correct to 3 significant digits
-    answers['(d) Weight influence'] = None
+    answers['(d) Weight influence'] = 1.528
     return answers
 
 
